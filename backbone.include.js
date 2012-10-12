4ea013eba4a967e29a02a0b73ccb7e9d63247f45
Anthony Short <antshort@gmail.com>
2012-10-12 15:31:42 +1100

    Added include to Backbone.Router. Bumped version

--- backbone.include.js
+++ backbone.include.js
@@ -1,10 +1,10 @@
-/*! backbone.include - v0.1.1 - 2012-10-12
+/*! backbone.include - v0.1.3 - 2012-10-12
 * https://github.com/anthonyshort/backbone.include
 * Copyright (c) 2012 Anthony Short; Licensed MIT */
 
 (function() {
 
-  Backbone.Model.include = Backbone.Collection.include = Backbone.View.include = module.exports = function(obj) {
+  Backbone.Model.include = Backbone.Collection.include = Backbone.View.include = Backbone.Router.include = module.exports = function(obj) {
     var key, value, _ref;
     for (key in obj) {
       value = obj[key];
